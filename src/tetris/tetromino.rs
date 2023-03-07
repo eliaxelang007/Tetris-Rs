ae4929ff21df77960cb40a521185efc042c1ef7b
Eli <eli.axel.ang@gmail.com>
2023-03-07 11:05:54 +0800

    uhhh

--- src/tetris/tetromino.rs
+++ src/tetris/tetromino.rs
@@ -1,1 +1,14 @@
-fn tet
+#[derive(Debug, Copy, Clone)]
+struct HalfStep(i8);
+
+impl HalfStep {
+    fn from<const POSITION: f32>() {
+        HalfStep(((POSITION.abs() * 2.0).round() * POSITION.signum()) as i8)
+    }
+}
+
+impl From<HalfStep> for f32 {
+    fn from(half_step: HalfStep) -> Self {
+        (half_step.0 as f32) / 2.0
+    }
+}
